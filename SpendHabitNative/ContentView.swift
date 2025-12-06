e9142718235c99ced18b90651ea36f50077d59c1
Angel <notnakuusemail@gmail.com>
2025-12-06 18:01:42 +0100

    Initial Commit

---
+++ SpendHabitNative/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpendHabitNative
+//
+//  Created by Angel Mariano Mishchanchuk on 6/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpendHabitNative/SpendHabitNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendHabitNativeApp.swift
+//  SpendHabitNative
+//
+//  Created by Angel Mariano Mishchanchuk on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SpendHabitNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
