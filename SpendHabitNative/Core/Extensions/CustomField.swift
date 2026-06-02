f97e56fb20279df8a6f848b39786628b9618f1d8
Angel <notnakuusemail@gmail.com>
2026-06-02 20:18:55 +0200

    user persona info update functionality

---
+++ SpendHabitNative/Core/Extensions/CustomField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomField.swift
+//  SpendHabitNative
+//
+//  Created by Angel Mariano Mishchanchuk on 02/06/2026.
+//
+
+import SwiftUI
+
+struct CustomField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomField()
+}
